1b2b51192fef4de829100f6519d8887c7ffd9959
Anton Wyrowski <anton.wyrowski@gmail.com>
2018-12-26 20:58:13 +0100

    Updates

---
+++ precache-manifest.aed4f6a9dcb3b5ada0149611b39eaddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a3077a8f691ddee1b5",
+    "url": "/grade-calculator/static/js/runtime~main.31a3077a.js"
+  },
+  {
+    "revision": "ca1362221562e2af8c56",
+    "url": "/grade-calculator/static/js/main.ca136222.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/grade-calculator/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "ca1362221562e2af8c56",
+    "url": "/grade-calculator/static/css/main.e18019c7.chunk.css"
+  },
+  {
+    "revision": "0eb5623d89762089e76e39a3e8c836fa",
+    "url": "/grade-calculator/index.html"
+  }
+];
